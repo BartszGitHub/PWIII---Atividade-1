2aafc6892c8df809e4a1775d7922ff086004690e
Bruno <brunohma@live.com>
2019-08-23 20:46:34 -0300

    exercicio6

---
+++ dir/exercicio6.js
@@ -0,0 +1,6 @@
+var largura = 5;
+var comprimento = 10;
+var area = function (xlargura, xcomprimento) {
+    return xlargura * xcomprimento;
+};
+console.log(area(largura, comprimento));
---
+++ exercicio6.ts
@@ -0,0 +1,11 @@
+let largura: number = 5
+let comprimento: number = 10
+
+let area = function(xlargura:number, xcomprimento:number): number {
+    return xlargura * xcomprimento }
+
+console.log(area(largura,comprimento))
+
+
+
+

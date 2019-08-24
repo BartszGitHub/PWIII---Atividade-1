ce843d8ff38989f52c7f7cb2e51a35cee87802fa
Bruno <brunohma@live.com>
2019-08-23 22:03:22 -0300

    exercicio10

---
+++ dir/exercicio10.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+exports.sobra = function (x2) {
+    if (x2 % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+exports.area1 = function (xlargura1, xcomprimento1) {
+    return xlargura1 * xcomprimento1;
+};
+exports.cubo1 = function (xncubo) {
+    return Math.pow(xncubo, 3);
+};
---
+++ exercicio10.ts
@@ -0,0 +1,25 @@
+export let sobra = function(x2:number): boolean {
+    if (x2 % 2 == 0){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+
+
+export let area1 = function(xlargura1:number, xcomprimento1:number): number {
+        return xlargura1 * xcomprimento1 }
+    
+
+
+
+export let cubo1 = (xncubo:number) => {
+    return Math.pow (xncubo,3)
+}
+
+
+
+
+

6092af482db7ada29821537155df4edaa56a3fb2
Bruno <brunohma@live.com>
2019-08-23 21:20:09 -0300

    exercicio7

---
+++ dir/exercicio7.js
@@ -0,0 +1,5 @@
+var ncubo = 3;
+var cubo = function (xncubo) {
+    return Math.pow(xncubo, 3);
+};
+console.log(cubo(ncubo));
---
+++ exercicio7.ts
@@ -0,0 +1,7 @@
+let ncubo: number = 3
+
+let cubo = (xncubo:number) => {
+    return Math.pow (xncubo,3)
+}
+
+console.log(cubo(ncubo))

17c3e147526fc1a90d8c7907d0438b1f0d113672
Bruno <brunohma@live.com>
2019-08-23 22:08:58 -0300

    exercicio11

---
+++ dir/exercicio11.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var exercicio10_1 = require("./exercicio10");
+var exercicio10_2 = require("./exercicio10");
+var exercicio10_3 = require("./exercicio10");
+if (exercicio10_1.sobra(12)) {
+    console.log("Par");
+}
+else {
+    console.log("Ímpar");
+}
+console.log(exercicio10_2.area1(5, 10));
+console.log(exercicio10_3.cubo1(3));
---
+++ exercicio11.ts
@@ -0,0 +1,16 @@
+import {sobra} from "./exercicio10";
+import {area1} from "./exercicio10";
+import {cubo1} from "./exercicio10";
+
+if (sobra(12)) {
+    console.log("Par")
+}
+else {
+    console.log("Ímpar")
+}
+
+
+console.log(area1(5,10))
+
+console.log(cubo1(3))
+
